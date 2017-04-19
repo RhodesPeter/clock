b0995e472a3224ab78598c5e43cf521512855b0a
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-19 14:03:30 +0100

    Add transition to hide clock on first load

--- index.js
+++ index.js
@@ -3,12 +3,20 @@
   var secondHand = document.getElementsByClassName("clock__second")[0];
   var minuteHand = document.getElementsByClassName("clock__minute")[0];
   var hourHand = document.getElementsByClassName("clock__hour")[0];
+  var clock = document.getElementsByClassName("clock")[0];
 
   function setDate(){
     var now = new Date();
 
     var seconds = now.getSeconds();
     var secondDegrees = ((seconds / 60) * 360) + 90;
+
+    if (seconds === 0){
+      secondHand.classList.add('no-transition');
+    } else {
+      secondHand.classList.remove('no-transition');
+    }
+
     secondHand.style.transform = 'rotate(' + secondDegrees + 'deg)';
 
     var minutes = now.getMinutes();
@@ -19,8 +27,12 @@
     if (hours > 12){ hours -= 12};
     var hourDegrees = (hours * 30) + ((minuteDegrees - 90) / 12) + 90;
     hourHand.style.transform = 'rotate(' + hourDegrees + 'deg)';
-   }
+  }
 
   setInterval(setDate, 1000);
+
+  window.setTimeout(function () {
+    clock.classList.remove('off-screen');
+  }, 1000);
 
 })();
