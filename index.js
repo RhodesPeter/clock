4d7872aa3d24587320640056d9cf50fbbb2fc436
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-19 19:35:15 +0100

    Add viewport meta tag

--- index.js
+++ index.js
@@ -4,10 +4,29 @@
   var minuteHand = document.getElementsByClassName("clock__minute")[0];
   var hourHand = document.getElementsByClassName("clock__hour")[0];
   var clock = document.getElementsByClassName("clock")[0];
+  var cityButton = document.getElementsByClassName("clock__city-button")[0];
+  var clockTitle = document.getElementsByClassName("clock__title")[0];
+  var index = 0;
+  var city = 'LONDON';
+  var cities = ["NEW YORK", "FREETOWN", "TOKYO", "LONDON"];
+
+  cityButton.addEventListener("click", nextCity);
+
+  function getTime(){
+    var timeNow = new Date();
+    var now = new Date ( timeNow );
+    var alt = {
+      "LONDON": 0,
+      "NEW YORK": 5,
+      "FREETOWN": 1,
+      "TOKYO": 16
+    };
+    now.setHours( timeNow.getHours() - alt[city] );
+    return now;
+  }
 
   function setDate(){
-    var now = new Date();
-
+    var now = getTime();
     var seconds = now.getSeconds();
     var secondDegrees = ((seconds / 60) * 360) + 90;
 
@@ -34,5 +53,13 @@
   window.setTimeout(function () {
     clock.classList.remove('off-screen');
   }, 1000);
+
+  function nextCity(){
+    var textnode = document.createTextNode(cities[index]);
+    clockTitle.replaceChild(textnode, clockTitle.childNodes[0]);
+    city = cities[index];
+    index = index === 3 ? 0 : index += 1;
+    cityButton.innerHTML = cities[index] + ' &rarr;';
+  }
 
 })();
